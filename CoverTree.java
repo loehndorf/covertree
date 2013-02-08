ffaf1f30b5c7771e8a4e8f2454efee58a550633e
Nils Loehndorf <nils@loehndorf.com>
2013-02-08 11:47:43 +0100

    Added disclaimer

--- CoverTree.java
+++ CoverTree.java
@@ -1,4 +1,31 @@
-package com.sdsolve.tools;
+/**
+ * This class provides a Java version of the cover tree nearest neighbor algorithm. It is based on Thomas Kollar's version of "Cover Trees for Nearest
+ * Neighbor" by John Langford, Sham Kakade and Alina Beygelzimer. The original algorithm is extended towards selecting K centers 
+ * which are maximally different from each other from an online sample.
+ * 
+ * Date of creation: 2013-02-08
+ * Copyright (c) 2013, Nils Loehndorf
+ * All rights reserved.
+ * 
+ * The software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ *    1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 
+ *    2. This notice may not be removed or altered from any source
+ *    distribution.
+ * 
+ * @author Nils Loehndorf
+ *
+ */
 
 import java.util.Collections;
 import java.util.LinkedList;
